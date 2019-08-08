eca6b6d3a0f8f42bae71f5207b9e858c7dc5584d
YKhemych <yosyp0108khemych@gmail.com>
2019-08-08 14:07:12 +0300

    Create user dto and interface, Get Put Post Delete methods

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
 import { ProductsController } from './products/products.controller';
 import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [UsersModule, ProductsModule],
-  controllers: [AppController, UsersController, ProductsController],
-  providers: [AppService, UsersService],
+  controllers: [AppController, ProductsController],
+  providers: [AppService],
 })
 export class AppModule {}
