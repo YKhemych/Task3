9d56c0c6ed974b343a054070fe30b40500539856
YKhemych <yosyp0108khemych@gmail.com>
2019-08-08 12:46:45 +0300

    Create users and products controllers, modules and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
+import { ProductsController } from './products/products.controller';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, ProductsModule],
+  controllers: [AppController, UsersController, ProductsController],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
